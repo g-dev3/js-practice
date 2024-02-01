9754eff609d424267df0180de6d28be0150c7060
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-01 07:46:52 +0000

    oprations of datatype

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -1,16 +1,35 @@
 let score = undefined;
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" number
 // "33abc" NaN 
 // true => 1
 // false => 0
 // "" => false
+
+// ----------*Oprations*---------
+
+let value = 3;
+
+let nagValue = -value;
+
+// console.log(nagValue);
+
+let str1= "G";
+let str2= " Develper";
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
 
 
