715a6a42c2b2545de8755f480282d0dcda1c13ab
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-01 06:42:38 +0000

    conversions of dataTypes

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,16 @@
+let score = undefined;
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" number
+// "33abc" NaN 
+// true => 1
+// false => 0
+// "" => false
+
+
