29a95292a7d7e88186b0f167f18e9fd86338a74d
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-01 08:22:13 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,47 @@
+// Primative dataType
+
+// 7
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// BigInt
+// Symbol
+
+
+let name = "G developer";
+let number = 21;
+let float = 21.2;
+let IsLoggin = true;
+let temp = null;
+let empty;
+let num = 3333333664444434n;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+
+
+// Reference type (non premitive)
+
+// Array
+// Object
+// functions
+
+
+const dogy= ["Robin","Tony","Robin2","kaniya","jasmin"];
+
+let dogys={
+    name:"Tony",
+    age:undefined
+}
+
+const dog = function(){
+    console.log("Robin");
+    console.log("Tony");
+    console.log("Kaniya");
+    console.log("Robin2");
+    console.log("Jasmin");
+}
