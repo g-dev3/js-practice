06cd8b52671615f105d3061a083c2906a7c100c6
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-02 06:44:26 +0000

    block and global scope value

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,47 @@ if(true){
     let a = 10;
     const b = 20;
     // var c = 30;
-    console.log("Inner ",a);
+    // console.log("Inner ",a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username= "Gdev";
+
+    function two(){
+        const website="youtube";
+        // console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+
+one();
+
+if(true){
+    const username = "gdev";
+    if(username==="gdev"){
+        const website ="youtube";
+        // console.log(username + website);
+
+    }
+    // console.log(website);
+
+}
+// console.log(username);
+
+// ----------intersting ---------
+
+console.log(addone(5));
+function addone(num){
+    return num+1;
+}
+
+
+addTwo(5);
+const addTwo = function(num){
+    return num +2;
+}
