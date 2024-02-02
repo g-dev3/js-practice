247e3de7f291e12aea798210ffa2280a1290494c
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-02 01:27:38 +0000

    function reference and excution

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,44 @@
+function sayMyName (){
+    console.log("g");
+    console.log("d");
+    console.log("e");
+    console.log("v");
+    console.log("e");
+    console.log("l");
+    console.log("o");
+    console.log("p");
+    console.log("e");
+    console.log("r");
+}
+// sayMyName();
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2);
+}
+
+function addTwoNumbers(num1,num2){
+    // let result = num1 + num2;
+    // return result;
+    return num1+num2;
+}
+// addTwoNumbers(3,4); // reference and exacue exacution
+
+const result = addTwoNumbers(6,3);
+
+// console.log("result",result);
+
+function loginUsermessage(username){
+    // if(username===undefined){
+    //     console.log("please enter a userName");
+    //     return
+
+    // }
+    if(!username){
+        console.log("please enter a userName");
+        return
+        
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUsermessage("G Dev"));
+console.log(loginUsermessage());
