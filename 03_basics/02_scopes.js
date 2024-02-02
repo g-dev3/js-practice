842abaa30421ff75140a536d750976858391e58d
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-02 06:13:03 +0000

    block scope and global scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+// block scope and global scope
+
+let a = 300;
+if(true){
+    let a = 10;
+    const b = 20;
+    // var c = 30;
+    console.log("Inner ",a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
