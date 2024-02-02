1f9637265072c455e384edd78c73e3b82cef3abe
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-02 06:00:32 +0000

    get reference data in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -41,4 +41,31 @@ function loginUsermessage(username = "gdev"){
     return `${username} just logged in`
 }
 // console.log(loginUsermessage("G Dev"));
-console.log(loginUsermessage("g developer"));
+// console.log(loginUsermessage("g developer"));
+
+
+
+function calculateCartPrice(val1,val2, ...num1){
+    return num1 
+}
+
+// console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username: "gdev",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject({username:"gdev",price:399});
+
+const myNewArray = [200,400,600];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
