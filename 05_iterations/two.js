3e8ffeefa590e812df6a415fcd3e0620ca80d943
G Developer <157769824+g-dev3@users.noreply.github.com>
2024-02-03 01:48:10 +0000

    while loop and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+/*
+let i=0;
+
+while (i<=10){
+    console.log(`value is i is ${i}`);
+    i+=2;
+}
+*/
+/*
+let myArray = ["Robin","Tony","Robin 2","Kaniya","Jasmin"];
+let arr=1;
+while(arr <= myArray.length){
+    console.log(`${arr} is ${myArray[arr-1]}`);
+    arr++;
+}
+*/
+
+/*
+let array = ["Robin", "Tony", "Robin 2", "kaniya", "Jasmin"];
+let index = 1;
+
+while (index <= array.length) {
+    console.log(index + " number is " + array[index - 1]);
+    index++;
+}
+*/
+let score = 11;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
